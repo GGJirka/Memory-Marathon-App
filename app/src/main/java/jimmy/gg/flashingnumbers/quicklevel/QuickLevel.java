b52453f4b7e27584e432cfad6118c62b8f24db4c
ggjimmy <jirkazboril4@seznam.cz>
2017-02-03 14:34:48 +0100

    Quick level updated

--- app/src/main/java/jimmy/gg/flashingnumbers/quicklevel/QuickLevel.java
+++ app/src/main/java/jimmy/gg/flashingnumbers/quicklevel/QuickLevel.java
@@ -1,18 +1,22 @@
 package jimmy.gg.flashingnumbers.quicklevel;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.PorterDuff;
-import android.os.Build;
 import android.os.CountDownTimer;
-import android.support.annotation.RequiresApi;
-import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.StringBuilderPrinter;
+import android.support.v7.view.ContextThemeWrapper;
+import android.text.Html;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -23,9 +27,16 @@ import java.util.Random;
 import jimmy.gg.flashingnumbers.R;
 
 public class QuickLevel extends AppCompatActivity {
+
     public ProgressBar progress;
+    public EditText editText;
     public TextView numbers;
+    public TextView onEndView;
+    private StringBuilder builder;
     private int level = 1;
+    private CountDownTimer firstTimer = null;
+    private CountDownTimer secondTimer = null;
+    private CountDownTimer thirdTimer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +46,10 @@ public class QuickLevel extends AppCompatActivity {
         setTitle("Quick Level");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         progress = (ProgressBar) findViewById(R.id.quick_progressBar);
+        onEndView = (TextView) findViewById(R.id.quick_on_end_view);
         rulesExplain(false);
         numbers = (TextView) findViewById(R.id.quick_numbers);
-
+        editText = (EditText) findViewById(R.id.quick_edit_text);
     }
 
     public void rulesExplain(boolean userDenied){
@@ -55,6 +67,7 @@ public class QuickLevel extends AppCompatActivity {
                     .show();
         }
     }
+
     public void timerBetweenLevels(){
         final TextView countDown = (TextView) findViewById(R.id.quick_count_down);
         countDown.setVisibility(View.VISIBLE);
@@ -78,7 +91,7 @@ public class QuickLevel extends AppCompatActivity {
         TextView timeRemain = (TextView) findViewById(R.id.quick_time_remain);
         timeRemain.setVisibility(View.VISIBLE);
         numbers.setVisibility(View.VISIBLE);
-        StringBuilder builder = new StringBuilder();
+        builder = new StringBuilder();
         Random random = new Random();
         numbers.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
@@ -103,33 +116,96 @@ public class QuickLevel extends AppCompatActivity {
             @Override
             public void onFinish() {
                 levelRemember();
-
             }
         }.start();
     }
 
     public void levelRemember(){
-        EditText editText = (EditText) findViewById(R.id.quick_edit_text);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
         editText.getBackground().setColorFilter(getResources().getColor(R.color.numbers_title_bot), PorterDuff.Mode.SRC_ATOP);
         numbers.setVisibility(View.INVISIBLE);
         editText.setVisibility(View.VISIBLE);
-        timerOnEnd();
+        editText.setFocusable(true);
+        editText.setFocusableInTouchMode(true);
+        onEndView.setVisibility(View.VISIBLE);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
+        timerOnEnd(editText);
     }
 
-    public void timerOnEnd(){
-        new CountDownTimer(level*5000,1){
-
+    public void timerOnEnd(final EditText editText){
+        new CountDownTimer(level*4000,1){
             @Override
             public void onTick(long millisUntilFinished) {
-                progress.setProgress((int) millisUntilFinished/(5000/100));
+                progress.setProgress((int) millisUntilFinished/(level*4000/100));
             }
-
             @Override
             public void onFinish() {
-                levelRemember();
-
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+                numberFinish();
             }
         }.start();
+    }
+    public void numberFinish(){
+        TextView timeRemain = (TextView) findViewById(R.id.quick_time_remain);
+        timeRemain.setVisibility(View.INVISIBLE);
+        progress.setVisibility(View.INVISIBLE);
+        onEndView.setVisibility(View.INVISIBLE);
+        editText.setVisibility(View.INVISIBLE);
+        String numberText = editText.getText().toString();
+        ContextThemeWrapper newContext = new ContextThemeWrapper(getApplicationContext(), R.style.MaterialButton);
+        final LinearLayout layout = (LinearLayout) findViewById(R.id.quick_linear_result);
+        layout.setGravity(Gravity.CENTER);
+        TextView textView1 = new TextView(QuickLevel.this);
+        TextView textView2 = new TextView(QuickLevel.this);
+        TextView number = new TextView(QuickLevel.this);
+        TextView answer = new TextView(QuickLevel.this);
+        textView1.setTextSize(20);
+        textView2.setTextSize(20);
+        number.setTextSize(24);
+        answer.setTextSize(24);
+        Button next = new Button(newContext);
+        textView1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        textView2.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        textView1.setText("Number");
+        textView2.setText("Your answer");
+        next.setText("NEXT");
+        number.setText(builder.toString());
+        try {
+            for (int i = 0; i < builder.length(); i++) {
+                if (numberText.charAt(i) != ' ') {
+                    char c = numberText.charAt(i);
+                    if (c == builder.charAt(i)) {
+                        String a = "<font color='#37FF8B'>" + c + "</font>";
+                        answer.append(Html.fromHtml(a));
+                    } else {
+                        String a = "<font color='#FE5F55'>" + c + "</font>";
+                        answer.append(Html.fromHtml(a));
+                    }
+                }
+            }
+        }catch (ArrayIndexOutOfBoundsException exception){
+            exception.printStackTrace();
+        }
+        layout.addView(textView1);
+        layout.addView(number);
+        layout.addView(textView2);
+        layout.addView(answer);
+        layout.addView(next);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                layout.removeAllViews();
+                level++;
+                timerBetweenLevels();
+            }
+        });
+        editText.setText("");
+    }
+    public void numbersRemembered(View view){
+        numberFinish();
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem){
