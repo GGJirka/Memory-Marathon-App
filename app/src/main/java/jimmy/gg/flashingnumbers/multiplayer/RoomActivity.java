fc54cd4e70f688a8fcb3134ebf67b4f8a4e1e057
ggjimmy <jirkazboril4@seznam.cz>
2017-03-19 21:22:43 +0100

    v1.2 - added english wordlist for words, update TODOs

--- app/src/main/java/jimmy/gg/flashingnumbers/multiplayer/RoomActivity.java
+++ app/src/main/java/jimmy/gg/flashingnumbers/multiplayer/RoomActivity.java
@@ -1,14 +1,11 @@
 package jimmy.gg.flashingnumbers.multiplayer;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import jimmy.gg.flashingnumbers.R;
@@ -17,6 +14,10 @@ import jimmy.gg.flashingnumbers.highscore.Score;
 import jimmy.gg.flashingnumbers.sockets.FakeClient;
 
 public class RoomActivity extends AppCompatActivity {
+    /*TODO: bug - connecting doesnt show up users in list,
+    * better design
+    * when someone start the room send to other
+    * display numbers to other*/
     public  final String ROOMNAME = "ROOMNAME";
     public  final String NICKNAME = "NICKNAME";
     public  FakeClient client;
@@ -50,8 +51,8 @@ public class RoomActivity extends AppCompatActivity {
     }
 
     public void click(View view){
-        client.setData(getRoomName(),this);
-        client.sendMessage("ROOMSTART "+getRoomName());
+        //client.setData(getRoomName(),this);
+        //client.sendMessage("ROOMSTART "+getRoomName());
     }
 
     public String getRoomName(){
